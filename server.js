b139be609953c6cce3a473697ffb9128e59a9f54
jcorozco81 <jcorozco@gmail.com>
2021-08-29 01:34:26 -0400

    Authentication support code

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const session = require('express-session');
+
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Esto es un secreto',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const hbs = exphbs.create({});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.use(routes);
 
 
 //START SERVER
