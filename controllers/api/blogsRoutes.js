6f766ce446653192b68fc87d278a48676e3c07bc
jcorozco81 <jcorozco@gmail.com>
2021-08-29 01:31:00 -0400

    Updated routes

--- controllers/api/blogsRoutes.js
+++ controllers/api/blogsRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { User, Comments, Blogs } = require("../../models");
+const withAuth = require("../../utils/auth");
+
 
 
 router.get('/', async (req, res) => {
@@ -20,9 +22,37 @@ router.get('/', async (req, res) => {
       }
     });
 
+    router.post('/', withAuth, async (req, res) => {
+      try {
+          const blogData = await Blogs.create(req.body);  
+  
+      
+          res.status(200).json(blogData);
+  
+        } catch (err) {
+          res.status(400).json(err);
+        }
+      });
 
-
-
+      router.delete('/:id', withAuth, async (req, res) => {
+        try {
+          const blogData = await Blogs.destroy({
+            where: {
+              id: req.params.id,
+              user_id: req.session.user_id,
+            },
+          });
+      
+          if (!blogData) {
+            res.status(404).json({ message: 'No project found with this id!' });
+            return;
+          }
+      
+          res.status(200).json(blogData);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      });
 
     
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User, Comments, Blogs } = require("../models");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
@@ -10,38 +11,60 @@ router.get("/", async (req, res) => {
 
     const blogList = blogData.map((blog) => blog.get({ plain: true }));
     console.log(blogList);
+    console.log(req.session.logged_in);
 
-    res.render("homepage", { blogList });
+    res.render("homepage", { blogList, logged_in: req.session.logged_in});
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get("/blog/:id", async (req, res) => {
+router.get("/blog/:id", withAuth, async (req, res) => {
   try {
     const getBlog = await Blogs.findByPk(req.params.id, {
       include: [{ model: User }, { model: Comments, include: [{ model: User }] }],
     });
     const blog = getBlog.get({ plain: true });
     const comms = blog.comments;
-console.log(comms);
-    res.render("blog", { ...blog, comms });
+console.log(req.session.user_id);
+    res.render("blog", { ...blog, comms, logged_in: req.session.logged_in, user_id: req.session.user_id});
   } catch (err) {
     res.status(400).json(err);
     
   }
 });
 
-router.get("/dashboard", async (req, res) => {
+router.get("/dashboard", withAuth, async (req, res) => {
   try {
-    const blogData = await Blogs.findAll({
+    const blogData = await Blogs.findAll({ where: {user_id: req.session.user_id},
+      // req.session.user_id
       include: [{ model: User }, { model: Comments }],
       // attributes: { exclude: ["time_id", "service_id"] },
     });
-    const blogs = blogData.get({ plain: true });
-    console.log(blogs);
+    const blogList = blogData.map((blog) => blog.get({ plain: true }));
+    console.log(blogList);
+    // res.status(200).json(blogList);
+    res.render("dashboard", { blogList, logged_in: req.session.logged_in, user_id: req.session.user_id});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-    res.render("dashboard", { ...blogs });
+router.get("/login", async (req, res) => {
+  try {
+ 
+
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/register", async (req, res) => {
+  try {
+ 
+
+    res.render("register");
   } catch (err) {
     res.status(500).json(err);
   }
