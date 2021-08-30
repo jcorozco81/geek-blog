ad5c4047634cfb413a650dfef76ab1e9f9c85995
jcorozco81 <jcorozco@gmail.com>
2021-08-30 01:03:20 -0400

    Added expiring time to cookies

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -38,6 +38,7 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.cookie.maxAge = 1 * 3600000;
       req.session.logged_in = true;
       console.log(req.session.logged_in);
       res.json({ user: userData, message: 'You are now logged in!' });
